8a0a736e6a6df72d7bf1d0264411a5526c3b6206
Saqlainkhadim <94662735+saqlainkhadimbusiness@users.noreply.github.com>
2022-09-08 02:52:07 +0500

    Authentication

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
